e6684c3b8aea39d4ee6a4dde325cb3aa282377a6
Bryson Tanner <bltanner105@gmail.com>
2025-04-13 13:50:44 -0500

    chore: Cleanup unused code

--- include/fluir/compiler/afg/block.hpp
+++ include/fluir/compiler/afg/block.hpp
@@ -1,46 +1,8 @@
 #ifndef FLUIR_COMPILER_AFG_BLOCK_HPP
 #define FLUIR_COMPILER_AFG_BLOCK_HPP
 
-#include <concepts>
-#include <memory>
-#include <vector>
+namespace fluir::afg {
 
-namespace fluir {
-  enum class Operator {
-    UNKNOWN,
-    PLUS,
-    MINUS,
-    STAR,
-    SLASH,
-  };
-
-  namespace afg {
-    class Node { };
-
-    using SharedNode = std::shared_ptr<Node>;
-    template <typename ConcreteNode>
-      requires std::derived_from<ConcreteNode, Node>
-    SharedNode shared(ConcreteNode node);
-
-    class Block {
-     public:
-      std::vector<SharedNode> pipelines;
-    };
-
-    class BinOp : public Node {
-     public:
-      Operator op;
-      SharedNode lhs;
-      SharedNode rhs;
-    };
-
-    class Constant : public Node {
-     public:
-      Type type;
-      Value value;
-    };
-
-  }  // namespace afg
-}  // namespace fluir
+}  // namespace fluir::afg
 
 #endif

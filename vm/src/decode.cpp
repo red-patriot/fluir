99f22f7494da1f7cda0db3bb51ac3668febf0948
Bryson Tanner <bltanner105@gmail.com>
2025-04-27 19:24:53 -0500

    feat: Decode generically based source type

--- vm/src/decode.cpp
+++ vm/src/decode.cpp
@@ -1,37 +1,16 @@
 #include "vm/decoder/decode.hpp"
 
-#include <charconv>
+#include <stdexcept>
 
+#include "vm/decoder/inspect.hpp"
 namespace fluir {
-  code::Header decodeHeader(std::string_view source) {
-    code::Header header;
-
-    header.filetype = source.at(0);
-
-    auto majorStr = source.substr(1, 2);
-    auto result = std::from_chars(majorStr.begin(), majorStr.end(),
-                                  header.major, 16);
-
-    auto minorStr = std::string_view(result.ptr, 2);
-    result = std::from_chars(minorStr.begin(), minorStr.end(),
-                             header.minor, 16);
-
-    auto patchStr = std::string_view(result.ptr, 2);
-    result = std::from_chars(patchStr.begin(), patchStr.end(),
-                             header.patch, 16);
-
-    auto entryOffsetStr = std::string_view(result.ptr, 16);
-    result = std::from_chars(entryOffsetStr.begin(),
-                             entryOffsetStr.end(),
-                             header.entryOffset, 16);
-
-    return header;
-  }
-  code::ByteCode decodeInspectCode(std::string_view source) {
-    code::ByteCode decoded{};
-
-    decoded.header = decodeHeader(source);
-
-    return decoded;
+  code::ByteCode decode(std::string_view source) {
+    if (source.size() >= 1) {
+      switch (source.at(0)) {
+        case 'I':
+          return InspectDecoder{}.decode(source);
+      }
+    }
+    throw std::runtime_error("Invalid header. File type must be 'I' (0x4C).");
   }
 }  // namespace fluir

0532d4036fcf9991ee96b810e1ff1514a48bd741
Bryson Tanner <bltanner105@gmail.com>
2025-05-05 21:48:31 -0500

    feat: Add buttons to the home page

--- editor/client/src/components/pages/Home.tsx
+++ editor/client/src/components/pages/Home.tsx
@@ -1,7 +1,21 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import viteLogo from '/electron-vite.animate.svg';
+import { faFileCirclePlus, faFilePen } from '@fortawesome/free-solid-svg-icons';
+
 export default function Home() {
   return (
-    <div>
-      <button>Open File</button>
+    <div className='flex flex-row'>
+      <img src={viteLogo} className='logo' />
+      <div className='flex flex-col grid content-evenly'>
+        <button>
+          <FontAwesomeIcon icon={faFileCirclePlus} />
+          New File
+        </button>
+        <button>
+          <FontAwesomeIcon icon={faFilePen} />
+          Open File
+        </button>
+      </div>
     </div>
   );
 }

18b01591f6eaa2885acb482784575715df3916ce
Bryson Tanner <bltanner105@gmail.com>
2025-06-01 13:10:23 -0500

    chore: Remove old zoom state

--- editor/client/src/store/programSlice.ts
+++ editor/client/src/store/programSlice.ts
@@ -6,12 +6,10 @@ export const MIN_ZOOM = 1;
 
 export interface ProgramState {
   module?: FluirModule | null;
-  zoom: number;
 }
 
 const initialState: ProgramState = {
   module: null,
-  zoom: 10,
 };
 
 export const programSlice = createSlice({
@@ -24,15 +22,8 @@ export const programSlice = createSlice({
     closeModule: (state) => {
       state.module = null;
     },
-    zoomIn: (state, action: PayloadAction<number>) => {
-      state.zoom = Math.min(state.zoom * action.payload, MAX_ZOOM);
-    },
-    zoomOut: (state, action: PayloadAction<number>) => {
-      state.zoom = Math.max(state.zoom / action.payload, MIN_ZOOM);
-    },
   },
 });
 
-export const { setOpenModule, closeModule, zoomIn, zoomOut } =
-  programSlice.actions;
+export const { setOpenModule, closeModule } = programSlice.actions;
 export default programSlice.reducer;

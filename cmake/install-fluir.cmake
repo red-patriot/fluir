5cea81524ce566a21b633c0bb9d3903607dfc093
Bryson Tanner <bltanner105@gmail.com>
2025-07-27 08:45:36 -0500

    build: Add CMake steps to build a ubuntu installer

---
+++ cmake/install-fluir.cmake
@@ -0,0 +1,23 @@
+include(GNUInstallDirs)
+
+function(configure_cpack)
+    set(CPACK_PACKAGE_NAME fluir)
+    set(CPACK_PACKAGE_VERSION ${PROJECT_VERSION})
+    set(CPACK_PACKAGE_CONTACT "bltanner105+fluir@gmail.com")
+    set(CPACK_PACKAGE_DESCRIPTION ${PROJECT_DESCRIPTION})
+    set(CPACK_DEBIAN_PACKAGE_DEPENDS "libfmt-dev")
+
+    include(CPack)
+endfunction()
+
+function(setup_fluir_install)
+    install(DIRECTORY artifacts/Launcher/ DESTINATION ${CMAKE_INSTALL_LIBEXECDIR}/fluir)
+    install(DIRECTORY artifacts/Editor-BE/ DESTINATION ${CMAKE_INSTALL_LIBEXECDIR}/fluir)
+    install(DIRECTORY artifacts/Editor-FE/linux-unpacked/
+            DESTINATION ${CMAKE_INSTALL_LIBEXECDIR}/fluir/editor-fe)
+    install(TARGETS fluir.compiler fluir.vm
+            DESTINATION ${CMAKE_INSTALL_LIBEXECDIR}/fluir)
+    install(FILES launcher/fluir-config.yaml DESTINATION ${CMAKE_INSTALL_LIBEXECDIR}/fluir)
+
+    configure_cpack()
+endfunction()

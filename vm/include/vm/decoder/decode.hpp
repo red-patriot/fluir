7ce50c504fc65b13e94c8037e748781c9a3c8be1
Bryson Tanner <bltanner105@gmail.com>
2025-08-13 07:42:42 -0500

    docs: Update documentation of functions

--- vm/include/vm/decoder/decode.hpp
+++ vm/include/vm/decoder/decode.hpp
@@ -6,7 +6,9 @@
 #include "bytecode/byte_code.hpp"
 
 namespace fluir {
+  /** Decodes the header from a source string */
   code::Header decodeHeader(std::string_view source);
+  /** Decodes a source string into runnable bytecode */
   code::ByteCode decode(std::string_view source);
 }  // namespace fluir
 

1d6cac579bcb55054d53bd5e917a00598cef7848
Bryson Tanner <bltanner105@gmail.com>
2025-08-17 22:15:46 -0500

    feat: Prevent running newer bytecode versions from the VM

--- vm/src/main.cpp
+++ vm/src/main.cpp
@@ -3,13 +3,19 @@
 #include <sstream>
 #include <type_traits>
 
+#include <bytecode/version.hpp>
+
 #include "vm/decoder/decode.hpp"
+#include "vm/validate.hpp"
 #include "vm/vm.hpp"
 
+static std::ostream& operator<<(std::ostream& os, const fluir::Version& version);
+
 int main(int argc, char** argv) {
+  fluir::Version current;  // Default construct to get the current language version
   // TODO: Make this work better and add other flags
   if (argc != 2) {
-    std::cerr << "Usage: fluir <file to execute>.\n";
+    std::cerr << "Fluir version " << current << "\nUsage: fluir <file to execute>.\n";
     return -1;
   }
 
@@ -19,9 +25,20 @@ int main(int argc, char** argv) {
   auto contents = ss.str();
 
   auto header = fluir::decodeHeader(contents);
-  // Check for version compatibility
+  if (fluir::Version fileVersion{header.major, header.minor, header.patch};
+      !fluir::checkVersionIsValid(current, fileVersion)) {
+    std::cerr << "Cannot run this bytecode. It has version " << fileVersion
+              << " which cannot be run by a VM of version " << current << '\n';
+    return -1;
+  }
+
   auto bytecode = fluir::decode(header, contents);
   fluir::VirtualMachine vm;
   auto result = vm.execute(&bytecode);
   return static_cast<std::underlying_type_t<fluir::ExecResult>>(result);
+}
+
+static std::ostream& operator<<(std::ostream& os, const fluir::Version& version) {
+  os << (int)version.major << '.' << (int)version.minor << '.' << (int)version.patch;
+  return os;
 }

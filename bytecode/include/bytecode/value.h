4db45d749473380d0b1ebd59a2254d99cf0ecc1b
Bryson Tanner <bltanner105@gmail.com>
2025-09-10 22:35:10 -0500

    refactor: use higher-order macro for value types

--- bytecode/include/bytecode/value.h
+++ bytecode/include/bytecode/value.h
@@ -5,59 +5,55 @@
 #include <stdexcept>
 
 namespace fluir::code {
-  enum class ValueType {
-    F64,
-    I64,
-    U64,
-  };
+  // clang-format off
+#define FLUIR_CODE_VALUE_TYPES(code)  \
+  code(F64, double)                   \
+  code(I64, std::int64_t)             \
+  code(U64, std::uint64_t)
+  // clang-format on
+
+#define enumerate(Type, ConcreteType) Type,
+  enum class ValueType { FLUIR_CODE_VALUE_TYPES(enumerate) };
+#undef enumerate
 
   /* A generic Fluir value */
   class Value {
    public:
-    explicit Value(double d) : type_{ValueType::F64}, data_{d} { }
-    explicit Value(std::int64_t i) : type_{ValueType::I64}, data_{i} { }
-    explicit Value(std::uint64_t u) : type_{ValueType::U64}, data_{u} { }
+#define FLUIR_VALUE_CONSTRUCTOR(Type, Concrete) \
+  explicit Value(Concrete d) : type_{ValueType::Type}, data_(d) { }
+
+    FLUIR_CODE_VALUE_TYPES(FLUIR_VALUE_CONSTRUCTOR)
+
+#undef FLUIR_VALUE_CONSTRUCTOR
 
     [[nodiscard]] ValueType type() const { return type_; };
 
-    [[nodiscard]] double& asF64() {
-      assertType(ValueType::F64);
-      return data_.f64;
-    }
-    [[nodiscard]] const double& asF64() const {
-      assertType(ValueType::F64);
-      return data_.f64;
-    }
+#define TEMP_CONCAT(a, b) a##b
+#define FLUIR_VALUE_ACCESSOR(Type, Concrete)                    \
+  [[nodiscard]] Concrete& TEMP_CONCAT(as, Type)() {             \
+    assertType(ValueType::Type);                                \
+    return data_.Type;                                          \
+  }                                                             \
+  [[nodiscard]] const Concrete& TEMP_CONCAT(as, Type)() const { \
+    assertType(ValueType::Type);                                \
+    return data_.Type;                                          \
+  }
 
-    [[nodiscard]] std::int64_t& asI64() {
-      assertType(ValueType::I64);
-      return data_.i64;
-    }
-    [[nodiscard]] const std::int64_t& asI64() const {
-      assertType(ValueType::I64);
-      return data_.i64;
-    }
+    FLUIR_CODE_VALUE_TYPES(FLUIR_VALUE_ACCESSOR)
 
-    [[nodiscard]] std::uint64_t& asU64() {
-      assertType(ValueType::U64);
-      return data_.u64;
-    }
-    [[nodiscard]] const std::uint64_t& asU64() const {
-      assertType(ValueType::U64);
-      return data_.u64;
-    }
+#undef FLUIR_VALUE_ACCESSOR
+#undef TEMP_CONCAT
 
    private:
     ValueType type_;
 
     union Data {
-      double f64;
-      std::int64_t i64;
-      std::uint64_t u64;
+#define FLUIR_VALUE_UNION_MEMBER(Type, Concrete) \
+  Concrete Type;                                 \
+  explicit(false) Data(Concrete d) : Type(d) { }
 
-      explicit(false) Data(double d) : f64(d) { }
-      explicit(false) Data(std::int64_t i) : i64(i) { }
-      explicit(false) Data(std::uint64_t u) : u64(u) { }
+      FLUIR_CODE_VALUE_TYPES(FLUIR_VALUE_UNION_MEMBER)
+#undef FLUIR_VALUE_UNION_MEMBER
     } data_;
 
     void assertType(ValueType type) const {
@@ -68,7 +64,7 @@ namespace fluir::code {
   };
 
   namespace value_literals {
-    inline `Value operator""_f64(long double d) { return Value{static_cast<double>(d)}; }
+    inline Value operator""_f64(long double d) { return Value{static_cast<double>(d)}; }
     inline Value operator""_i64(unsigned long long i) { return Value{static_cast<std::int64_t>(i)}; }
     inline Value operator""_u64(unsigned long long int i) { return Value{static_cast<std::uint64_t>(i)}; }
 

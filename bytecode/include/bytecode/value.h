bf72e11ea0af2517fa77367c444f9cc3329265ae
Bryson Tanner <bltanner105@gmail.com>
2025-09-10 21:24:18 -0500

    feat: Add U64 as a type for a Value

--- bytecode/include/bytecode/value.h
+++ bytecode/include/bytecode/value.h
@@ -1,24 +1,78 @@
 #ifndef FLUIR_VALUE_H
 #define FLUIR_VALUE_H
 
+#include <cstdint>
+#include <stdexcept>
+
 namespace fluir::code {
   enum class ValueType {
-    FLOAT64,
+    F64,
+    I64,
+    U64,
   };
 
   /* A generic Fluir value */
   class Value {
    public:
-    /* implicit */ Value(double d) : data_(d) { }
+    explicit Value(double d) : type_{ValueType::F64}, data_{d} { }
+    explicit Value(std::int64_t i) : type_{ValueType::I64}, data_{i} { }
+    explicit Value(std::uint64_t u) : type_{ValueType::U64}, data_{u} { }
 
-    double& asFloat64() { return data_; }
-    const double& asFloat64() const { return data_; }
+    [[nodiscard]] ValueType type() const { return type_; };
 
-    ValueType type() const { return ValueType::FLOAT64; }
+    [[nodiscard]] double& asF64() {
+      assertType(ValueType::F64);
+      return data_.f64;
+    }
+    [[nodiscard]] const double& asF64() const {
+      assertType(ValueType::F64);
+      return data_.f64;
+    }
+
+    [[nodiscard]] std::int64_t& asI64() {
+      assertType(ValueType::I64);
+      return data_.i64;
+    }
+    [[nodiscard]] const std::int64_t& asI64() const {
+      assertType(ValueType::I64);
+      return data_.i64;
+    }
+
+    [[nodiscard]] std::uint64_t& asU64() {
+      assertType(ValueType::U64);
+      return data_.u64;
+    }
+    [[nodiscard]] const std::uint64_t& asU64() const {
+      assertType(ValueType::U64);
+      return data_.u64;
+    }
 
    private:
-    double data_;
+    ValueType type_;
+
+    union Data {
+      double f64;
+      std::int64_t i64;
+      std::uint64_t u64;
+
+      explicit(false) Data(double d) : f64(d) { }
+      explicit(false) Data(std::int64_t i) : i64(i) { }
+      explicit(false) Data(std::uint64_t u) : u64(u) { }
+    } data_;
+
+    void assertType(ValueType type) const {
+      if (type_ != type) {
+        throw std::runtime_error("Actual value type does not match");
+      }
+    }
   };
+
+  namespace value_literals {
+    inline `Value operator""_f64(long double d) { return Value{static_cast<double>(d)}; }
+    inline Value operator""_i64(unsigned long long i) { return Value{static_cast<std::int64_t>(i)}; }
+    inline Value operator""_u64(unsigned long long int i) { return Value{static_cast<std::uint64_t>(i)}; }
+
+  }  // namespace value_literals
 }  // namespace fluir::code
 
 #endif  // FLUIR_VALUE_H
--- bytecode/test/value.test.cpp
+++ bytecode/test/value.test.cpp
@@ -1,27 +1,55 @@
 #include "bytecode/value.h"
 
+#include <cstdint>
 #include <tuple>
 
 #include <gtest/gtest.h>
 
 using std::tuple;
 
-TEST(TestValue, InitializeFromDouble) {
+using namespace fluir::code::value_literals;
+
+TEST(TestValue, InitializeFloat64) {
   double expected = 4.5;
 
-  fluir::code::Value value = expected;
+  fluir::code::Value value{expected};
 
-  EXPECT_DOUBLE_EQ(expected, value.asFloat64());
+  ASSERT_EQ(fluir::code::ValueType::F64, value.type());
+  EXPECT_DOUBLE_EQ(expected, value.asF64());
 }
 
-class TestValueRemembersType : public ::testing::TestWithParam<tuple<fluir::code::ValueType, fluir::code::Value>> { };
+TEST(TestValue, InitializeInt64) {
+  std::int64_t expected = 31;
 
-TEST_P(TestValueRemembersType, Test) {
-  const auto& [type, value] = GetParam();
+  fluir::code::Value value{expected};
 
-  EXPECT_EQ(type, value.type());
+  ASSERT_EQ(fluir::code::ValueType::I64, value.type());
+  EXPECT_DOUBLE_EQ(expected, value.asI64());
 }
 
-INSTANTIATE_TEST_SUITE_P(,
-                         TestValueRemembersType,
-                         ::testing::Values(tuple{fluir::code::ValueType::FLOAT64, fluir::code::Value{12.5}}));
+TEST(TestValue, InitializeUint64) {
+  std::uint64_t expected = 12345;
+
+  fluir::code::Value value{expected};
+
+  ASSERT_EQ(fluir::code::ValueType::U64, value.type());
+  EXPECT_DOUBLE_EQ(expected, value.asU64());
+}
+
+TEST(TestValue, I64AccessChecksForFloat) {
+  fluir::code::Value value = 4.5_f64;
+
+  EXPECT_THROW(auto _ = value.asI64(), std::runtime_error);
+}
+
+TEST(TestValue, F64AccessChecksForFloat) {
+  fluir::code::Value value = 12_u64;
+
+  EXPECT_THROW(auto _ = value.asF64(), std::runtime_error);
+}
+
+TEST(TestValue, U64AccessChecksForFloat) {
+  fluir::code::Value value = 12_i64;
+
+  EXPECT_THROW(auto _ = value.asU64(), std::runtime_error);
+}

f54672122fc37af38d3d495774ff3463af0c37dd
Bryson Tanner <bltanner105@gmail.com>
2025-09-27 13:10:59 -0500

    feat: Add utility function to update an operator

--- editor/client/src/components/flow_diagram/logic/tests/updateNode.test.ts
+++ editor/client/src/components/flow_diagram/logic/tests/updateNode.test.ts
@@ -1,6 +1,6 @@
 import { describe, it, expect, vi } from 'vitest';
-import { updateConstant } from '@/components/flow_diagram/logic/updateNode';
-import { UpdateConstantEditRequest } from '@/models/edit_request';
+import { updateConstant, updateOperator } from '@/components/flow_diagram/logic/updateNode';
+import { UpdateConstantEditRequest, UpdateOperatorEditRequest } from '@/models/edit_request';
 
 describe('updateConstant', () => {
   it('Contains the correct id', () => {
@@ -32,6 +32,40 @@ describe('updateConstant', () => {
       discriminator: 'update_constant',
       target: [0, 1, 2],
       value: '145',
+    } as UpdateConstantEditRequest);
+  });
+});
+
+describe('updateOperator', () => {
+  it('Contains the correct id', () => {
+    const commitMock = vi.fn();
+    const id = '0:1:2';
+
+    const uut = updateOperator(commitMock, id);
+
+    uut('-');
+
+    expect(commitMock).toHaveBeenCalledOnce();
+    expect(commitMock).toHaveBeenLastCalledWith({
+      discriminator: 'update_operator',
+      target: [0, 1, 2],
+      value: '-',
+    } as UpdateOperatorEditRequest);
+  });
+
+  it('Sends the correct values', () => {
+    const commitMock = vi.fn();
+    const id = '0:1:2';
+
+    const uut = updateConstant(commitMock, id);
+
+    uut('*');
+
+    expect(commitMock).toHaveBeenCalledOnce();
+    expect(commitMock).toHaveBeenLastCalledWith({
+      discriminator: 'update_constant',
+      target: [0, 1, 2],
+      value: '*',
     } as UpdateConstantEditRequest);
   });
 });
--- editor/client/src/components/flow_diagram/logic/updateNode.ts
+++ editor/client/src/components/flow_diagram/logic/updateNode.ts
@@ -1,4 +1,8 @@
-import { UpdateConstantEditRequest } from '@/models/edit_request';
+import {
+  UpdateConstantEditRequest,
+  UpdateOperatorEditRequest,
+} from '@/models/edit_request';
+import { Operator } from '@/models/fluir_module';
 import { toApiID } from '@/utility/idHelpers';
 
 export function updateConstant(
@@ -8,6 +12,20 @@ export function updateConstant(
   return (value: string) => {
     const request: UpdateConstantEditRequest = {
       discriminator: 'update_constant',
+      target: toApiID(fullID),
+      value,
+    };
+    commit(request);
+  };
+}
+
+export function updateOperator(
+  commit: (request: UpdateOperatorEditRequest) => void,
+  fullID: string,
+) {
+  return (value: Operator) => {
+    const request: UpdateOperatorEditRequest = {
+      discriminator: 'update_operator',
       target: toApiID(fullID),
       value,
     };

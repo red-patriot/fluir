5f694225f1d469cc2aefb74aa3fbab5ec80d323c
Bryson Tanner <bltanner105@gmail.com>
2025-11-01 16:57:40 -0500

    feat: It is not ambiguous for a cast to have explicit and implicit options

--- compiler/test/types/overload_resolution.test.cpp
+++ compiler/test/types/overload_resolution.test.cpp
@@ -3,6 +3,8 @@
 #include "compiler/types/symbol_table.hpp"
 
 using fluir::Operator;
+using fluir::types::OperatorDefinition;
+using fluir::types::Type;
 
 class TestOverloadResolution : public ::testing::Test {
  public:
@@ -16,38 +18,31 @@ class TestOverloadResolution : public ::testing::Test {
     G = uut.addType(fluir::types::Type{"G"});
     H = uut.addType(fluir::types::Type{"H"});
 
-    uut.addOperator(fluir::types::OperatorDefinition{A, Operator::PLUS, A, A});   // A + A => A
-    uut.addOperator(fluir::types::OperatorDefinition{A, Operator::MINUS, A, A});  // A - A => A
-    uut.addOperator(fluir::types::OperatorDefinition{A, Operator::SLASH, B, C});  // A + B => C
-    uut.addOperator(fluir::types::OperatorDefinition{A, Operator::STAR, B, C});   // A - B => C
-    uut.addOperator(fluir::types::OperatorDefinition{A, Operator::PLUS, C, C});   // A + C => C
-    uut.addOperator(fluir::types::OperatorDefinition{A, Operator::MINUS, C, C});  // A - C => C
-    uut.addOperator(fluir::types::OperatorDefinition{D, Operator::PLUS, D, D});   // C + D => D
-    uut.addOperator(fluir::types::OperatorDefinition{D, Operator::MINUS, D, D});  // C - D => D
-    uut.addOperator(fluir::types::OperatorDefinition{C, Operator::PLUS, C, C});   // C + C => C
-    uut.addOperator(fluir::types::OperatorDefinition{C, Operator::MINUS, C, C});  // C - C => C
-    uut.addOperator(fluir::types::OperatorDefinition{Operator::MINUS, A, A});     // + A => A
-    uut.addOperator(fluir::types::OperatorDefinition{Operator::PLUS, A, A});      // - A => A
-    uut.addOperator(fluir::types::OperatorDefinition{E, Operator::PLUS, E, E});   // E + E => E
-    uut.addOperator(fluir::types::OperatorDefinition{E, Operator::MINUS, E, E});  // E - E => E
-    uut.addOperator(fluir::types::OperatorDefinition{E, Operator::STAR, E, E});   // E * E => E
-    uut.addOperator(fluir::types::OperatorDefinition{E, Operator::SLASH, E, E});  // E / E => E
-    uut.addOperator(fluir::types::OperatorDefinition{Operator::MINUS, E, E});     // - E => E
-    uut.addOperator(fluir::types::OperatorDefinition{Operator::PLUS, E, E});      // + E => E
-    uut.addOperator(fluir::types::OperatorDefinition{F, Operator::PLUS, F, G});   // F + F => G
-    uut.addOperator(fluir::types::OperatorDefinition{F, Operator::MINUS, F, G});  // F - F => G
-    uut.addOperator(fluir::types::OperatorDefinition{F, Operator::STAR, F, G});   // F * F => G
-    uut.addOperator(fluir::types::OperatorDefinition{F, Operator::SLASH, F, G});  // F / F => G
-    uut.addOperator(fluir::types::OperatorDefinition{Operator::PLUS, F, F});      // - F => G
+    uut.addOperator(OperatorDefinition{A, Operator::PLUS, A, A});   // A + A => A
+    uut.addOperator(OperatorDefinition{A, Operator::MINUS, A, A});  // A - A => A
+    uut.addOperator(OperatorDefinition{Operator::PLUS, A, A});      // + A   => A
+    uut.addOperator(OperatorDefinition{Operator::MINUS, A, A});     // - A   => A
+    uut.addOperator(OperatorDefinition{D, Operator::PLUS, D, D});   // D + D => D
+    uut.addOperator(OperatorDefinition{D, Operator::MINUS, D, D});  // D - D => D
+    uut.addOperator(OperatorDefinition{Operator::PLUS, C, C});      // + C   => C
+    uut.addOperator(OperatorDefinition{Operator::MINUS, D, D});     // - D   => D
+    uut.addOperator(OperatorDefinition{E, Operator::STAR, F, F});   // E * F => F
+    uut.addOperator(OperatorDefinition{E, Operator::STAR, D, E});   // E * D => E
+    uut.addOperator(OperatorDefinition{A, Operator::SLASH, D, A});  // A / D => A
+    uut.addOperator(OperatorDefinition{A, Operator::SLASH, F, F});  // A / F => F
+    uut.addOperator(OperatorDefinition{Operator::PLUS, F, F});      // + F   => F
+    uut.addOperator(OperatorDefinition{Operator::MINUS, F, F});     // - F   => F
+    uut.addOperator(OperatorDefinition{A, Operator::SLASH, H, C});  // A / H => C
+    uut.addOperator(OperatorDefinition{Operator::MINUS, H, H});     // - H   => H
 
-    uut.addExplicitConversion(D, E);  // D (e)=> E
-    uut.addImplicitConversion(A, B);  // A (i)=> B
-    uut.addExplicitConversion(C, D);  // C (e)=> D
-    uut.addImplicitConversion(E, F);  // E (i)=> F
-    uut.addImplicitConversion(D, A);  // D (i)=> A
-    uut.addImplicitConversion(G, F);  // G (i)=> F
-    uut.addImplicitConversion(G, E);  // G (i)=> E
-    uut.addImplicitConversion(D, C);  // D (i)=> C
+    uut.addImplicitConversion(B, A);  // B (i)-> A
+    uut.addImplicitConversion(E, D);  // E (i)-> D
+    uut.addImplicitConversion(E, F);  // E (i)-> F
+    uut.addExplicitConversion(A, D);  // A (e)-> D
+    uut.addExplicitConversion(G, C);  // G (e)-> C
+    uut.addImplicitConversion(G, D);  // G (i)-> D
+    uut.addImplicitConversion(H, F);  // H (i)-> F
+    uut.addExplicitConversion(G, F);  // G (e)-> F
   }
 
   fluir::types::SymbolTable uut;
@@ -62,8 +57,8 @@ class TestOverloadResolution : public ::testing::Test {
   fluir::types::Type const* H;
 };
 
-TEST_F(TestOverloadResolution, ResolvesBinaryWithNoConversions) {
-  const fluir::types::OperatorDefinition expected{A, Operator::PLUS, A, A};
+TEST_F(TestOverloadResolution, ResolvesBinaryNoCast) {
+  const OperatorDefinition expected{A, Operator::PLUS, A, A};
 
   const auto actual = uut.selectOverload(A, Operator::PLUS, A);
 
@@ -71,83 +66,98 @@ TEST_F(TestOverloadResolution, ResolvesBinaryWithNoConversions) {
   EXPECT_EQ(expected, *actual);
 }
 
-TEST_F(TestOverloadResolution, ResolvesUnaryWithNoConversions) {
-  const fluir::types::OperatorDefinition expected{Operator::PLUS, A, A};
+TEST_F(TestOverloadResolution, ResolvesUnaryNoCast) {
+  const OperatorDefinition expected{Operator::MINUS, A, A};
 
-  const auto actual = uut.selectOverload(Operator::PLUS, A);
-
-  ASSERT_TRUE(actual);
-  EXPECT_EQ(expected, *actual);
-}
-
-TEST_F(TestOverloadResolution, ResolvesBinaryWithDifferentTypes) {
-  const fluir::types::OperatorDefinition expected{A, Operator::SLASH, B, C};
-
-  const auto actual = uut.selectOverload(A, Operator::SLASH, B);
+  const auto actual = uut.selectOverload(Operator::MINUS, A);
 
   ASSERT_TRUE(actual);
   EXPECT_EQ(expected, *actual);
 }
 
-TEST_F(TestOverloadResolution, ResolvesBinaryWithRhsCast) {
-  const fluir::types::OperatorDefinition expected{A, Operator::SLASH, B, C};
+TEST_F(TestOverloadResolution, ResolvesUnary1Cast) {
+  const OperatorDefinition expected{Operator::PLUS, A, A};
 
-  const auto actual = uut.selectOverload(A, Operator::SLASH, A);
-
-  ASSERT_TRUE(actual);
-  EXPECT_EQ(expected, *actual);
-}
-
-TEST_F(TestOverloadResolution, ResolvesBinaryWithLhsCast) {
-  const fluir::types::OperatorDefinition expected{F, Operator::PLUS, F, G};
-
-  const auto actual = uut.selectOverload(E, Operator::PLUS, F);
+  const auto actual = uut.selectOverload(Operator::PLUS, B);
 
   ASSERT_TRUE(actual);
   EXPECT_EQ(expected, *actual);
 }
 
-TEST_F(TestOverloadResolution, ResolvesUnaryWithSingleCast) {
-  const fluir::types::OperatorDefinition expected{Operator::MINUS, E, E};
+TEST_F(TestOverloadResolution, ResolvesBinaryFirstCast) {
+  const OperatorDefinition expected{A, Operator::PLUS, A, A};
 
-  const auto actual = uut.selectOverload(Operator::MINUS, G);
-
-  ASSERT_TRUE(actual);
-  EXPECT_EQ(expected, *actual);
-}
-
-TEST_F(TestOverloadResolution, ResolvesBinaryWithMultipleCastOptions) {
-  const fluir::types::OperatorDefinition expected{E, Operator::MINUS, E, E};
-
-  const auto actual = uut.selectOverload(E, Operator::MINUS, G);
+  const auto actual = uut.selectOverload(B, Operator::PLUS, A);
 
   ASSERT_TRUE(actual);
   EXPECT_EQ(expected, *actual);
 }
 
-TEST_F(TestOverloadResolution, ResolvesUnaryWithConversion) {
-  const fluir::types::OperatorDefinition expected{Operator::MINUS, E, E};
+TEST_F(TestOverloadResolution, ResolvesBinarySecondCast) {
+  const OperatorDefinition expected{A, Operator::MINUS, A, A};
 
-  const auto actual = uut.selectOverload(Operator::MINUS, G);
+  const auto actual = uut.selectOverload(A, Operator::MINUS, B);
 
   ASSERT_TRUE(actual);
   EXPECT_EQ(expected, *actual);
 }
 
-TEST_F(TestOverloadResolution, FailsBinaryWithNoViableCandidates) {
-  const auto actual = uut.selectOverload(H, Operator::PLUS, H);
+TEST_F(TestOverloadResolution, FailsIfMultipleCastsRequired) {
+  const auto actual = uut.selectOverload(B, Operator::PLUS, B);
 
-  ASSERT_FALSE(actual);
+  EXPECT_FALSE(actual);
 }
 
-TEST_F(TestOverloadResolution, FailsBinaryWithAmbiguousCall) {
-  const auto actual = uut.selectOverload(H, Operator::PLUS, H);
+TEST_F(TestOverloadResolution, FailsBinaryIfNoViableCandidates) {
+  const auto actual = uut.selectOverload(C, Operator::PLUS, C);
 
-  ASSERT_FALSE(actual);
+  EXPECT_FALSE(actual);
 }
 
-TEST_F(TestOverloadResolution, FailsUnaryWithAmbiguousCall) {
+TEST_F(TestOverloadResolution, FailsUnaryIfNoViableCandidates) {
+  const auto actual = uut.selectOverload(Operator::MINUS, C);
+
+  EXPECT_FALSE(actual);
+}
+
+TEST_F(TestOverloadResolution, FailsBinaryIfCallIsAmbiguous) {
+  const auto actual = uut.selectOverload(E, Operator::STAR, E);
+
+  EXPECT_FALSE(actual);
+}
+
+TEST_F(TestOverloadResolution, FailsUnaryIfCallIsAmbiguous) {
+  const auto actual = uut.selectOverload(Operator::MINUS, E);
+
+  EXPECT_FALSE(actual);
+}
+
+TEST_F(TestOverloadResolution, FailsBinaryIfCallRequiresExplicitConversions) {
+  const auto actual = uut.selectOverload(E, Operator::STAR, A);
+
+  EXPECT_FALSE(actual);
+}
+
+TEST_F(TestOverloadResolution, FailsUnaryIfCallRequiresExplicitConversions) {
   const auto actual = uut.selectOverload(Operator::PLUS, G);
 
-  ASSERT_FALSE(actual);
+  EXPECT_FALSE(actual);
+}
+
+TEST_F(TestOverloadResolution, ResolvingExplicitAndImplicitBinaryAcceptsOnlyImplicit) {
+  const OperatorDefinition expected{A, Operator::SLASH, D, A};
+
+  const auto actual = uut.selectOverload(A, Operator::SLASH, G);
+
+  ASSERT_TRUE(actual);
+  EXPECT_EQ(expected, *actual);
+}
+
+TEST_F(TestOverloadResolution, ResolvingExplicitAndImplicitUnaryAcceptsOnlyImplicit) {
+  const OperatorDefinition expected{Operator::MINUS, D, D};
+
+  const auto actual = uut.selectOverload(Operator::MINUS, G);
+
+  ASSERT_TRUE(actual);
+  EXPECT_EQ(expected, *actual);
 }

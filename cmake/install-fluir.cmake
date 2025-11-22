1802242b8e84e7d0655ec165155115db4b679780
Bryson Tanner <bltanner105@gmail.com>
2025-11-22 12:35:31 -0600

    install: Add dummy postinst script to DEB package

--- cmake/install-fluir.cmake
+++ cmake/install-fluir.cmake
@@ -1,6 +1,6 @@
 include(GNUInstallDirs)
 
-function(configure_cpack)
+function (configure_cpack)
     set(CPACK_PACKAGE_NAME fluir)
     set(CPACK_PACKAGE_VERSION ${PROJECT_VERSION})
     set(CPACK_PACKAGE_CONTACT "bltanner105+fluir@gmail.com")
@@ -8,21 +8,34 @@ function(configure_cpack)
     set(CPACK_DEBIAN_PACKAGE_DEPENDS "libfmt-dev")
 
     include(CPack)
-endfunction()
+endfunction ()
 
-function(setup_fluir_install)
-    install(DIRECTORY artifacts/Launcher/
-            DESTINATION ${CMAKE_INSTALL_BINDIR}
-            USE_SOURCE_PERMISSIONS)
-    install(DIRECTORY artifacts/Editor-BE/
-            DESTINATION ${CMAKE_INSTALL_LIBEXECDIR}/fluir
-            USE_SOURCE_PERMISSIONS)
-    install(DIRECTORY artifacts/Editor-FE/linux-unpacked/
-            DESTINATION ${CMAKE_INSTALL_LIBEXECDIR}/fluir/editor-fe
-            USE_SOURCE_PERMISSIONS)
+function (setup_fluir_install)
+    install(
+        DIRECTORY artifacts/Launcher/
+        DESTINATION ${CMAKE_INSTALL_BINDIR}
+        USE_SOURCE_PERMISSIONS
+    )
+    install(
+        DIRECTORY artifacts/Editor-BE/
+        DESTINATION ${CMAKE_INSTALL_LIBEXECDIR}/fluir
+        USE_SOURCE_PERMISSIONS
+    )
+    install(
+        DIRECTORY artifacts/Editor-FE/linux-unpacked/
+        DESTINATION ${CMAKE_INSTALL_LIBEXECDIR}/fluir/editor-fe
+        USE_SOURCE_PERMISSIONS
+    )
     install(TARGETS fluir.compiler fluir.vm
-            DESTINATION ${CMAKE_INSTALL_LIBEXECDIR}/fluir)
-    install(FILES launcher/fluir-config.yaml DESTINATION ${CMAKE_INSTALL_DATAROOTDIR}/fluir)
+            DESTINATION ${CMAKE_INSTALL_LIBEXECDIR}/fluir
+    )
+    install(FILES launcher/fluir-config.yaml
+            DESTINATION ${CMAKE_INSTALL_DATAROOTDIR}/fluir
+    )
+
+    set(CPACK_DEBIAN_PACKAGE_CONTROL_EXTRA
+        "${CMAKE_SOURCE_DIR}/packaging/debian/postinst"
+    )
 
     configure_cpack()
-endfunction()
+endfunction ()

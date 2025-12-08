8b66e3072975d25f61bbeff6dcc0685aa6472cf4
Bryson Tanner <bltanner105@gmail.com>
2025-12-07 20:44:14 -0600

    fix: ++,-- are not binary operators

--- editor/client/src/components/flow_diagram/elements/OperatorNode.tsx
+++ editor/client/src/components/flow_diagram/elements/OperatorNode.tsx
@@ -38,7 +38,7 @@ export function BinaryOperatorNode({
       <ValueDisplay
         fullID={fullID}
         value={operator.op}
-        renderEdit={editWithChoicePopover(onSelect, ['+', '-', '*', '/', '++', '--'])}
+        renderEdit={editWithChoicePopover(onSelect, ['+', '-', '*', '/'])}
       />
       <DragHandle />
       <NodeOutput

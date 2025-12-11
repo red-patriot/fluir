e085e9588adc8d934a48a19b08a7cd65eec006f7
Bryson Tanner <bltanner105@gmail.com>
2025-12-11 12:12:08 -0600

    feat: add simple ostream sink to print errors to the terminal

---
+++ compiler/include/compiler/utility/diagnostic/ostream_sink.hpp
@@ -0,0 +1,36 @@
+#ifndef FLUIR_COMPILER_UTILITY_DIAGNOSTIC_OSTREAM_SINK_HPP
+#define FLUIR_COMPILER_UTILITY_DIAGNOSTIC_OSTREAM_SINK_HPP
+
+#include <ostream>
+
+#include "compiler/utility/diagnostic/sink.hpp"
+
+namespace fluir::diagnostic {
+  /** Prints error messages as strings to a std::ostream*/
+  class OstreamSink : public Sink {
+   public:
+    /** Initializes an ostream sink to write to std::cout */
+    OstreamSink();
+    explicit OstreamSink(std::ostream& os);
+    OstreamSink(OstreamSink const&) = delete;
+    OstreamSink& operator=(OstreamSink const&) = delete;
+    OstreamSink(OstreamSink&&) = delete;
+    OstreamSink& operator=(OstreamSink&&) = delete;
+    ~OstreamSink() override = default;
+
+    void operator()(const int& lineNo);
+    void operator()(const FullID& id);
+
+   private:
+    std::ostream& os_;
+
+    void report(Code code,
+                const std::filesystem::path& file,
+                const Sink::ErrorLocation& location,
+                std::string_view msg) override;
+
+    void printLocation(const Sink::ErrorLocation& location);
+  };
+}  // namespace fluir::diagnostic
+
+#endif
---
+++ compiler/src/utility/diagnostic/ostream_sink.cpp
@@ -0,0 +1,44 @@
+#include "compiler/utility/diagnostic/ostream_sink.hpp"
+
+#include <iostream>
+
+#include <fmt/format.h>
+
+#include "compiler/utility/diagnostic/pretty_msg.hpp"
+#include "compiler/utility/diagnostics.hpp"
+
+namespace fluir::diagnostic {
+  namespace {
+    std::string_view levelOf(Code code) {
+      if (code >= Code::GENERIC_ERROR) {
+        return "[ERROR]";
+      } else if (code >= Code::GENERIC_WARNING) {
+        return "[WARNING]";
+      } else {
+        return "[NOTE]";
+      }
+    }
+  }  // namespace
+
+  OstreamSink::OstreamSink() : OstreamSink(std::cout) { }
+  OstreamSink::OstreamSink(std::ostream& os) : os_(os) { }
+
+  void OstreamSink::operator()(const int& lineNo) { os_ << fmt::format("at line {}", lineNo); }
+  void OstreamSink::operator()(const FullID& id) { os_ << fmt::format(" at element {}", fmt::join(id, ":")); }
+
+  void OstreamSink::report(Code code,
+                           const std::filesystem::path& file,
+                           const Sink::ErrorLocation& location,
+                           std::string_view msg) {
+    os_ << fmt::format("[] in '{}' ", levelOf(code), file.string());
+    printLocation(location);
+    os_ << "\n\t" << prettyMessage(code);
+    if (!msg.empty()) {
+      os_ << "\n\t" << msg;
+    }
+    os_ << '\n';
+  }
+
+  void OstreamSink::printLocation(const Sink::ErrorLocation& location) { std::visit(*this, location); }
+
+}  // namespace fluir::diagnostic

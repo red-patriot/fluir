da0e9a764323cfd28e97142847949b5a0f772e43
Bryson Tanner <bltanner105@gmail.com>
2025-12-21 11:33:22 -0600

    feat: Implement contains for a trie with 2 levels

--- utility/include/fluir/util/trie.hpp
+++ utility/include/fluir/util/trie.hpp
@@ -4,9 +4,10 @@
 #include <algorithm>
 #include <concepts>
 #include <initializer_list>
-#include <memory>
 #include <string>
 #include <utility>
+#include <variant>
+#include <vector>
 
 namespace fluir::util {
   template <std::copyable T>
@@ -18,28 +19,35 @@ namespace fluir::util {
     using const_reference = const value_type&;
 
     Trie(const_reference defaultValue, const std::initializer_list<std::pair<key_type, value_type>>& entries);
+    ~Trie();
 
     const_reference at(key_type key) const;
     bool contains(key_type key) const;
 
    private:
+    struct Node;
+    using nodes = std::vector<Node*>;
+
     struct Node {
       std::string chars;
-      value_type value;
+      std::variant<nodes, value_type> value;
     };
 
     value_type default_{};
-    std::unique_ptr<Node> root_{};
+    Node* root_{};
 
-    void insert(const key_type& key, const value_type& value, Node* node);
+    void insert(key_type key, const value_type& value, Node* node);
     value_type const* findRest(const key_type& rest, Node* node) const;
+    void makeInternal(Node* node);
+
+    void recursivelyDelete(Node* node);
   };
 
   template <std::copyable T>
   Trie<T>::Trie(const_reference defaultValue, const std::initializer_list<std::pair<key_type, value_type>>& entries) :
-    default_(defaultValue), root_(std::make_unique<Node>()) {
+    default_(defaultValue), root_(new Node{}) {
     for (const auto& [key, value] : entries) {
-      insert(key, value, root_.get());
+      insert(key, value, root_);
     }
   }
 
@@ -49,15 +57,37 @@ namespace fluir::util {
   }
 
   template <std::copyable T>
-  bool Trie<T>::contains(key_type key) const {
-    return findRest(key, root_.get()) != nullptr;
+  Trie<T>::~Trie() {
+    if (root_) {
+      recursivelyDelete(root_);
+    }
   }
 
   template <std::copyable T>
-  void Trie<T>::insert(const key_type& key, const value_type& value, Node* node) {
+  bool Trie<T>::contains(key_type key) const {
+    return findRest(key, root_) != nullptr;
+  }
+
+  template <std::copyable T>
+  void Trie<T>::insert(key_type key, const value_type& value, Node* node) {
     if (node->chars.empty()) {
       node->chars = key;
       node->value = value;
+    } else {
+      if (std::holds_alternative<nodes>(node->value)) {
+        auto& children = std::get<nodes>(node->value);
+        auto firstChar = std::ranges::find(node->chars, key[0]);
+        if (firstChar == node->chars.end()) {
+          node->chars.push_back(key[0]);
+          children.emplace_back(new Node{std::string{key.substr(1)}, value});
+        } else {
+          auto index = std::distance(node->chars.begin(), firstChar);
+          insert(key.substr(1), value, children[index]);
+        }
+      } else {
+        makeInternal(node);
+        insert(key, value, node);
+      }
     }
   }
 
@@ -66,10 +96,42 @@ namespace fluir::util {
     if (!node) {
       return nullptr;
     }
-    if (node->chars == rest) {
-      return &node->value;
+    if (std::holds_alternative<value_type>(node->value)) {
+      if (rest != node->chars) {
+        return nullptr;
+      }
+      auto& value = std::get<value_type>(node->value);
+      return &value;
+    } else {
+      auto& children = std::get<nodes>(node->value);
+      auto firstChar = std::ranges::find(node->chars, rest[0]);
+      if (firstChar == node->chars.end()) {
+        return nullptr;
+      }
+      auto index = std::distance(node->chars.begin(), firstChar);
+      return findRest(rest.substr(1), children[index]);
     }
     return nullptr;
+  }
+
+  template <std::copyable T>
+  void Trie<T>::makeInternal(Node* node) {
+    auto child = new Node{};
+    child->chars = node->chars.substr(1);
+    child->value = std::move(node->value);
+    node->chars = node->chars[0];
+    node->value = nodes{child};
+  }
+
+  template <std::copyable T>
+  void Trie<T>::recursivelyDelete(Node* node) {
+    if (std::holds_alternative<nodes>(node->value)) {
+      auto& children = std::get<nodes>(node->value);
+      for (auto child : children) {
+        recursivelyDelete(child);
+      }
+    }
+    delete node;
   }
 }  // namespace fluir::util
 #endif
--- utility/test/trie.test.cpp
+++ utility/test/trie.test.cpp
@@ -10,3 +10,28 @@ TEST(TestTrie, ContainsWithOneElement) {
   EXPECT_TRUE(uut.contains("af"));
   EXPECT_FALSE(uut.contains("a"));
 }
+
+TEST(TestTrie, ContainsWithMultipleElementsOfSameLength) {
+  Trie<int> uut{0, {{"af", 1}, {"bf", 1}, {"cf", 1}, {"df", 1}}};
+
+  EXPECT_TRUE(uut.contains("af"));
+  EXPECT_TRUE(uut.contains("bf"));
+  EXPECT_TRUE(uut.contains("cf"));
+  EXPECT_TRUE(uut.contains("df"));
+  EXPECT_FALSE(uut.contains("ab"));
+  EXPECT_FALSE(uut.contains("a"));
+}
+
+TEST(TestTrie, ContainsWithMultipleElementsOfDifferingLengths) {
+  Trie<int> uut{0, {{"abc", 1}, {"hello", 1}, {"abdd", 1}, {"ad", 1}, {"stuff", 1}}};
+
+  EXPECT_TRUE(uut.contains("abc"));
+  EXPECT_TRUE(uut.contains("hello"));
+  EXPECT_TRUE(uut.contains("abdd"));
+  EXPECT_TRUE(uut.contains("ad"));
+  EXPECT_TRUE(uut.contains("stuff"));
+  EXPECT_FALSE(uut.contains("ab"));
+  EXPECT_FALSE(uut.contains("hell"));
+  EXPECT_FALSE(uut.contains("a"));
+  EXPECT_FALSE(uut.contains("asdf"));
+}

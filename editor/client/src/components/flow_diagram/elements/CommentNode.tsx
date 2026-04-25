845d7a7821316681dbc758c16994d1c355ae03fd
Bryson Tanner <bltanner105@gmail.com>
2026-04-25 16:30:37 -0400

    feat: Implement editing comments

--- editor/client/src/components/flow_diagram/elements/CommentNode.tsx
+++ editor/client/src/components/flow_diagram/elements/CommentNode.tsx
@@ -1,14 +1,33 @@
-import { Node, NodeProps } from "@xyflow/react";
-import { Comment } from "@/models/fluir_module";
-import { Box, Flex, Code } from "@radix-ui/themes";
-import { mauve } from "@radix-ui/colors";
-import ElementTag from "@/components/flow_diagram/common/ElementTag.tsx";
+import { Node, NodeProps } from '@xyflow/react';
+import { Comment } from '@/models/fluir_module';
+import { Box, Flex } from '@radix-ui/themes';
+import { mauve } from '@radix-ui/colors';
+import ElementTag from '@/components/flow_diagram/common/ElementTag.tsx';
+import { useProgramActions } from '@/components/reusable/ProgramActionsContext.tsx';
+import { updateComment } from '@/components/flow_diagram/logic/updateNode.ts';
+import { MultilineValueDisplay } from '@/components/flow_diagram/common/ValueDisplay.tsx';
+import DragHandle from '@/components/flow_diagram/common/DragHandle.tsx';
+import { editWithMultilineInputField } from '@/components/flow_diagram/common/InputField.tsx';
+import { XYResizeHandle } from '@/components/flow_diagram/common/ResizeHandle.tsx';
 
-type CommentNode = Node<{ comment: Comment; fullId: string }>;
+type CommentNode = Node<{ comment: Comment; fullID: string }>;
 
 export default function CommentNode({
-  data: { comment },
-}: NodeProps<CommentNode>) {
+                                      data: { comment, fullID },
+                                    }: NodeProps<CommentNode>) {
+  const { editProgram } = useProgramActions();
+
+  const updateValue = updateComment(editProgram, fullID);
+
+  const doEdit = editWithMultilineInputField({
+    validate: () => true,
+    onValidateSucceed: (text) => {
+      if (text != comment.data) {
+        updateValue(text);
+      }
+    },
+  });
+
   return (
     <Flex
       direction="column"
@@ -19,15 +38,14 @@ export default function CommentNode({
       <Flex direction="row" width="100%">
         <ElementTag name="//" />
         <Box className="grow" />
+        <DragHandle />
       </Flex>
-      <Code
-        className="grow w-full wrap-normal overflow-hidden text-ellipsis"
-        variant="solid"
-        color="gray"
-        size="2"
-      >
-        {comment.data}
-      </Code>
+      <MultilineValueDisplay
+        fullID={fullID}
+        value={comment.data}
+        renderEdit={doEdit}
+      />
+      <XYResizeHandle fullID={fullID} />
     </Flex>
   );
 }

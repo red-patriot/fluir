2c59e762b5c87998c77f09c53586034e282033f1
Bryson Tanner <bltanner105@gmail.com>
2026-06-14 15:18:30 -0400

    feat: Add and remove function call args

--- editor/client/src/components/flow_diagram/elements/CallNode.tsx
+++ editor/client/src/components/flow_diagram/elements/CallNode.tsx
@@ -14,6 +14,8 @@ import { renameCallArg } from '@/components/flow_diagram/logic/updateNode';
 import { editWithInputField } from '@/components/flow_diagram/common/InputField';
 import { validateDeclName } from '@/components/flow_diagram/logic/validateEdit.ts';
 import EditRequest from '@/models/edit_request';
+import { ContextMenu } from '@radix-ui/themes';
+import { addCallArg, deleteCallArg } from '@/components/flow_diagram/logic/updateNode';
 
 export type CallNode = Node<{
   call: Call;
@@ -25,29 +27,44 @@ export default function CallNode({
                                    selected,
                                  }: NodeProps<CallNode>) {
   const { editProgram } = useProgramActions();
+
+  const addArg = addCallArg(editProgram, fullID);
+
   return (
     <Flex
       direction="column"
       height="100%"
       align="center"
       style={{ backgroundColor: sky.sky11 }}
+      onContextMenu={(e) => e.stopPropagation()}
     >
-      <Flex direction="row" className="w-full">
-        <ElementTag name="fn" />
-        <ValueDisplay fullID={fullID} value={call.target} />
-        <DragHandle />
-      </Flex>
+      <ContextMenu.Root>
+        <ContextMenu.Trigger asChild>
+          <Flex direction="row" className="w-full">
+            <ElementTag name="fn" />
+            <ValueDisplay fullID={fullID} value={call.target} />
+            <DragHandle />
+          </Flex>
+        </ContextMenu.Trigger>
+        <ContextMenu.Content>
+          <ContextMenu.Item onSelect={() => addArg(`arg${call.arguments.length}`)}>Add Argument</ContextMenu.Item>
+        </ContextMenu.Content>
+      </ContextMenu.Root>
       <Flex direction="column" className="w-full">
         {call.arguments.map((arg, index) => (
-          <CallArgumentNode arg={arg} callID={fullID} index={index} editProgram={editProgram} />
+          <CallArgumentNode key={`${fullID}:${index}`} arg={arg} callID={fullID} index={index}
+                            editProgram={editProgram} />
         ))}
       </Flex>
-      {call.returns && <NodeOutput fullID={fullID} count={1} />}
-      {selected && (
-        <HorizontalResizeHandle fullID={fullID} minWidth={LIMITS.call.width.min * ZOOM_SCALAR} />
-      )}
-    </Flex>
-  );
+      {
+        call.returns && <NodeOutput fullID={fullID} count={1} />
+      }
+      {
+        selected && (
+          <HorizontalResizeHandle fullID={fullID} minWidth={LIMITS.call.width.min * ZOOM_SCALAR} />
+        )
+      }
+    </Flex>);
 }
 
 interface CallArgumentProps {
@@ -60,21 +77,33 @@ interface CallArgumentProps {
 function CallArgumentNode({ arg, callID, index, editProgram }: CallArgumentProps) {
   const updateName = renameCallArg(editProgram, callID, index);
   const doEdit = editWithInputField({ validate: validateDeclName, onValidateSucceed: updateName });
+  const deleteArg = deleteCallArg(editProgram, callID);
   return (
-    <Flex direction="row" className="w-full relative">
-      <Handle
-        position={Position.Left}
-        type="target"
-        id={`output-${callID}-${index}`}
-        style={{
-          backgroundColor: gray.gray10,
-          top: '50%',
-          left: 0,
-          right: 'auto',
-          transform: 'translate(-50%, -50%)',
-        }}
-      />
-      <ValueDisplay fullID={callID} value={arg} renderEdit={doEdit} />
-    </Flex>
+    <ContextMenu.Root>
+      <ContextMenu.Trigger asChild>
+        <Flex direction="row"
+              className="w-full relative"
+              onContextMenu={(e) => e.stopPropagation()}>
+          <Handle
+            position={Position.Left}
+            type="target"
+            id={`output-${callID}-${index}`}
+            style={{
+              backgroundColor: gray.gray10,
+              top: '50%',
+              left: 0,
+              right: 'auto',
+              transform: 'translate(-50%, -50%)',
+            }}
+          />
+          <ValueDisplay fullID={callID} value={arg} renderEdit={doEdit} />
+        </Flex>
+      </ContextMenu.Trigger>
+      <ContextMenu.Content>
+        <ContextMenu.Item onSelect={() => deleteArg(index)}>
+          Delete
+        </ContextMenu.Item>
+      </ContextMenu.Content>
+    </ContextMenu.Root>
   );
 }
